d890f46bb4e3265364672ff1b3cd1fe135a0c46f
shashwotbhattarai <shashwot.tech@gmail.com>
2024-05-30 17:16:40 +0545

    array prefixSum

---
+++ arr.prefixSum.js
@@ -0,0 +1,16 @@
+let a = [2, 5, 6, 7, 9];
+let sumarr = [];
+let i = 0;
+
+for (let i = 0; i < a.length; i++) {
+	let sum = 0;
+
+	if (i == 0) {
+		sumarr[i] = a[i];
+	} else {
+		let j = i - 1;
+		sum = sumarr[j] + a[i];
+		sumarr[i] = sum;
+	}
+}
+console.log(sumarr);
